bac642970d4a683cc626ffb2ea485a5a5b8e64bd
Ye Nay Thway <102862836+yenaythway@users.noreply.github.com>
2023-03-10 09:04:24 +0630

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,37 @@ database=database.DataBase()
 #         try:
 #             message = client.recv(1024).decode('ascii')
 #             print(message)
-#         except Exception as error:
-#             print(error)
+#             if message.startswith('Private Chat'):
+#                 obj_name = message.split('(')[1].split(')')[0]
+#                 message = message.split('): ')[1]
+#                 sub_name = names[clients.index(client)]
+#                 client_obj = clients[names.index(obj_name)]
+#                 client_obj.send(f"{sub_name}:{message}".encode('ascii'))
+#                 client.send(f"{sub_name} : {message}".encode('ascii'))
+#
+#             elif message.startswith('Group Chat'):
+#
+#                 clients_name=message.split('/')[1].split(',')
+#
+#                 message=message.split('/')[2]
+#
+#                 sub_name = names[clients.index(client)]
+#
+#                 client.send(f"{sub_name} : {message}".encode('ascii'))
+#                 for name in clients_name:
+#                     clients[names.index(name)].send(f"{sub_name} : {message}".encode('ascii'))
+#
+#             else:
+#                 client.send("Invalid message format.".encode('ascii'))
+#
+#         except:
+#             index = clients.index(client)
+#             clients.remove(client)
+#             client.close()
+#             name = names[index]
+#             broadcast(f'{name} left the chat'.encode('ascii'))
+#             names.remove(name)
+#             break
 
 
 def receive():
@@ -22,13 +51,14 @@ def receive():
         client, address = sever.accept()
         # connect_time = datetime.datetime.now()
         print(' connected')
+
         avilable_food=database.req_food()
         print(avilable_food)
         client.send(avilable_food.encode('ascii'))
         ordered_food=client.recv(1024).decode('ascii')
         shop_price=database.find_shop(ordered_food)
         client.send(shop_price.encode('ascii'))
-        # name = client.recv(1024).decode('ascii')
+        # # name = client.recv(1024).decode('ascii')
         #
         # thread = threading.Thread(target=handle, args=(client,))
         # thread.start()
